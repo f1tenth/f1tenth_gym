2273a13d3ef324b2e3747280e4ff7f514a580fb1
Benjamin <benjaminevans316@gmail.com>
2021-03-18 12:24:02 +0000

    Added the ability to add obstacles on the centerline

--- gym/f110_gym/envs/f110_env.py
+++ gym/f110_gym/envs/f110_env.py
@@ -37,6 +37,7 @@ from f110_gym.envs.laser_models import get_dt
 # others
 import numpy as np
 import os
+import csv
 import time
 
 # gl
@@ -186,7 +187,6 @@ class F110Env(gym.Env, utils.EzPickle):
         # rendering
         self.renderer = None
         self.current_obs = None
-
 
     def __del__(self):
         """
@@ -363,6 +363,34 @@ class F110Env(gym.Env, utils.EzPickle):
 
         self.renderer.add_obstacles(obs_locations_m, obs_size_m)
 
+    def load_centerline(self, file_name=None):
+        """
+        Loads a centerline from a csv file. 
+
+        Args:
+            file_name (string): the name of a csv file with the centerline of the track in the form [x_i, y_i, w_l_i, w_r_i], location and width
+
+        Returns:
+            center_pts (np.ndarray): the loaded center point location
+            widths (np.ndarray): the widths of the track
+        """
+        if file_name is None:
+            file_name = self.map_path + self.map_name + '_centerline.csv'
+        else:
+            file_name = self.map_path + file_name
+
+        track = []
+        with open(file_name, 'r') as csvfile:
+            csvFile = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  
+            for lines in csvFile:  
+                track.append(lines)
+        track = np.array(track)
+
+        center_pts = track[:, 0:2]
+        widths = track[:, 2:4]
+
+        return center_pts, widths
+
     def add_obstacles_centerline(self, n_obstacles, obstacle_size=[0.5, 0.5]):
         """
         Adds a set number of obstacles to the envioronment. 
@@ -381,32 +409,30 @@ class F110Env(gym.Env, utils.EzPickle):
         obs_size_m = np.array(obstacle_size)
         obs_size_px = obs_size_m / self.map_resolution
 
-        #TODO: find a way to get the centerline and then add obstacles along the centerline
-
         obs_locations = []
-        while len(obs_locations) < n_obstacles:
-            rand_x = int(np.random.random() * (self.map_width - obs_size_px[0]))
-            rand_y = int(np.random.random() * (self.map_height - obs_size_px[1]))
+        center_pts, widths = self.load_centerline()
 
-            if self.original_dt[rand_y, rand_x] > 0.05:
-                obs_locations.append([rand_y, rand_x])
+        # randomly select certain idx's
+        rand_idxs = np.random.randint(1, len(center_pts)-1)
+        
+        # randomly select location within box of minimum_width around the center point
+        rands = np.random.uniform(-1, 1, size=(n_obstacles, 2))
+        min_widths = np.min(widths, axis=0)
+        rand_xs = rands[:, 0] * min_widths
+        rand_ys = rands[:, 1] * min_widths
+        rand_boxes = np.concatenate([rand_xs, rand_ys], axis=-1)
+        obs_locations + center_pts[rand_idxs, :] + rand_boxes
 
-        #TODO: vectorise this
+        # change the values of the img at each obstacle location
         obs_locations = np.array(obs_locations)
         for location in obs_locations:
             x, y = location[0], location[1]
             map_img[x:x+obs_size_px[0], y:y+obs_size_px[1]] = 0
 
-
-        # obs_locations = np.array(obs_locations)
-        # for location in obs_locations:
-        #     x, y = location[0], location[1]
-        #     for i in range(0, int(obs_size_px[0])):
-        #         for j in range(0, int(obs_size_px[1])):
-        #             map_img[x+i, y+j] = 0
-
+        # update the image in the simulator
         self.sim.update_map_img(map_img)
 
+        # if rendering is on, then add obstacles to the renderer
         if self.renderer is not None:
             obstacle_x = obs_locations[:, 0] * self.map_resolution + self.orig_y + obs_size_m[0] / 2
             obstacle_y = obs_locations[:, 1] * self.map_resolution + self.orig_x + obs_size_m[1] / 2
