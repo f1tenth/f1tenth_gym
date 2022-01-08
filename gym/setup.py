f42526b1bdde91717a8e297256ad8cb34b8db3fc
nandant <nandan.t@live.com>
2022-01-08 13:30:54 -0500

    Library updates for M1 architecture

--- gym/setup.py
+++ gym/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='f110_gym',
-      version='0.2.1',
+      version='0.2.2',
       author='Hongrui Zheng',
       author_email='billyzheng.bz@gmail.com',
       url='https://f1tenth.org',
       install_requires=['gym==0.19.0',
-                        'numpy==1.20.0',
+                        'numpy==1.22.0',
                         'Pillow>=8.3.2',
-                        'scipy==1.7.1',
+                        'scipy<1.8.0',
                         'numba==0.54.1',
                         'pyyaml>=5.4',
                         'pyglet',
