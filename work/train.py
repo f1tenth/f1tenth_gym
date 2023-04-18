ae4c512fb418040d211ab3bc754bdbec8a807f0e
Meraj <meraccos@unist.ac.kr>
2023-04-18 21:42:25 +0900

    back to ppo

--- work/train.py
+++ work/train.py
@@ -2,9 +2,9 @@ from stable_baselines3 import PPO
 from utils import create_env, TensorboardCallback
 
 save_interval = 50_000
-save_path = "./models/ppo_model/ent_001"
+save_path = "./models/PPO_model"
 log_dir = "./metrics/"
-maps = list(range(1,180))
+maps = list(range(1,250))
 
 env = create_env(maps=maps)
 
@@ -13,8 +13,8 @@ model = PPO("MultiInputPolicy",
             env,
             verbose=2,
             # n_steps=2048,
-            ent_coef=0.001,
-            learning_rate=0.0001,
+            # ent_coef=0.001,
+            # learning_rate=0.0001,
             # vf_coef=0.5,
             # max_grad_norm=0.5,
             # gae_lambda=0.95,
@@ -31,5 +31,4 @@ combined_callback = TensorboardCallback(save_interval, save_path, verbose=1)
 
 model.learn(total_timesteps=10000_000, callback=combined_callback, progress_bar=True)
 
-env.env.close()
 env.close()
