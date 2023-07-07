6fe92b86b06c90bd0a27ac9d9f4718a291bc1a8d
luigiberducci <luigi.berd@gmail.com>
2023-07-07 12:10:50 +0200

    update test to match new env signature

--- gym/f110_gym/unittest/gym_api_test.py
+++ gym/f110_gym/unittest/gym_api_test.py
@@ -13,14 +13,24 @@ class TestEnvInterface(unittest.TestCase):
         from gymnasium.utils.env_checker import check_env
         import gymnasium as gym
 
-        example_dir = pathlib.Path(__file__).parent.parent.parent.parent / 'examples'
+        example_dir = pathlib.Path(__file__).parent.parent.parent.parent / "examples"
 
-        with open(example_dir / 'config_example_map.yaml') as file:
+        with open(example_dir / "config_example_map.yaml") as file:
             conf_dict = yaml.load(file, Loader=yaml.FullLoader)
         conf = Namespace(**conf_dict)
         conf.map_path = str(example_dir / conf.map_path)
 
-        env = gym.make('f110_gym:f110-v0', map=conf.map_path, map_ext=conf.map_ext, num_agents=1, timestep=0.01,
-                       integrator=Integrator.Euler)
+        env = gym.make(
+            "f110_gym:f110-v0",
+            config={
+                "map": conf.map_path,
+                "map_ext": conf.map_ext,
+                "num_agents": 1,
+                "timestep": 0.01,
+                "integrator": "rk4",
+                "control_input": "speed",
+                "params": {"mu": 1.0},
+            },
+        )
 
         check_env(env.unwrapped)
