4c4b25e38d4ccac88130c4eeab221dbdc8a0b7e0
Luigi Berducci <luigi.berd@gmail.com>
2023-08-31 17:50:27 +0200

    add rendering config file, update render spec loading params from file, add rendering steering angle, remove unused pyglet tests, update setup dependencies

--- gym/f110_gym/envs/f110_env.py
+++ gym/f110_gym/envs/f110_env.py
@@ -176,8 +176,7 @@ class F110Env(gym.Env):
         # stateful observations for rendering
         self.render_obs = None
         self.render_mode = render_mode
-        self.render_spec = RenderSpec(render_mode=self.render_mode)
-        self.renderer = make_renderer(track=self.track, render_spec=self.render_spec)
+        self.renderer = make_renderer(track=self.track, render_mode=render_mode)
 
     @classmethod
     def default_config(cls) -> dict:
@@ -319,6 +318,7 @@ class F110Env(gym.Env):
             "poses_x": self.sim.agent_poses[:, 0],
             "poses_y": self.sim.agent_poses[:, 1],
             "poses_theta": self.sim.agent_poses[:, 2],
+            "steering_angles": self.sim.agent_steerings,
             "lap_times": self.lap_times,
             "lap_counts": self.lap_counts,
             "collisions": self.sim.collisions,
