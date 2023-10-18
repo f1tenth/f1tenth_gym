9b536b2939d68cde1055387d410597e45a71b30a
Luigi Berducci <luigi.berd@gmail.com>
2023-10-18 10:21:00 +0200

    black

--- gym/f110_gym/test/test_f110_env.py
+++ gym/f110_gym/test/test_f110_env.py
@@ -23,10 +23,7 @@ class TestEnvInterface(unittest.TestCase):
         }
         conf = deep_update(conf, config)
 
-        env = gym.make(
-            "f110_gym:f110-v0",
-            config=conf,
-        )
+        env = gym.make("f110_gym:f110-v0", config=conf)
         return env
 
     def test_gymnasium_api(self):
@@ -159,8 +156,13 @@ class TestEnvInterface(unittest.TestCase):
         Test that the environment can be used in a vectorized environment.
         """
         num_envs, num_agents = 3, 2
-        config = {"num_agents": num_agents, "observation_config": {"type": "kinematic_state"}}
-        vec_env = gym.vector.make("f110_gym:f110-v0", asynchronous=False, config=config, num_envs=num_envs)
+        config = {
+            "num_agents": num_agents,
+            "observation_config": {"type": "kinematic_state"},
+        }
+        vec_env = gym.vector.make(
+            "f110_gym:f110-v0", asynchronous=False, config=config, num_envs=num_envs
+        )
 
         rnd_poses = np.random.random((2, 3))
         obss, infos = vec_env.reset(options={"poses": rnd_poses})
@@ -168,7 +170,13 @@ class TestEnvInterface(unittest.TestCase):
         for i, agent_id in enumerate(obss):
             for ie in range(num_envs):
                 agent_obs = obss[agent_id]
-                agent_pose = np.array([agent_obs["pose_x"][ie], agent_obs["pose_y"][ie], agent_obs["pose_theta"][ie]])
+                agent_pose = np.array(
+                    [
+                        agent_obs["pose_x"][ie],
+                        agent_obs["pose_y"][ie],
+                        agent_obs["pose_theta"][ie],
+                    ]
+                )
                 self.assertTrue(
                     np.allclose(agent_pose, rnd_poses[i]),
                     f"pose of agent {agent_id} in env {ie} should be {rnd_poses[i]}, got {agent_pose}",
@@ -179,8 +187,13 @@ class TestEnvInterface(unittest.TestCase):
         Test that the environment can be used in a vectorized environment.
         """
         num_envs, num_agents = 3, 2
-        config = {"num_agents": num_agents, "observation_config": {"type": "kinematic_state"}}
-        vec_env = gym.vector.make("f110_gym:f110-v0", asynchronous=True, config=config, num_envs=num_envs)
+        config = {
+            "num_agents": num_agents,
+            "observation_config": {"type": "kinematic_state"},
+        }
+        vec_env = gym.vector.make(
+            "f110_gym:f110-v0", asynchronous=True, config=config, num_envs=num_envs
+        )
 
         rnd_poses = np.random.random((2, 3))
         obss, infos = vec_env.reset(options={"poses": rnd_poses})
@@ -188,7 +201,13 @@ class TestEnvInterface(unittest.TestCase):
         for i, agent_id in enumerate(obss):
             for ie in range(num_envs):
                 agent_obs = obss[agent_id]
-                agent_pose = np.array([agent_obs["pose_x"][ie], agent_obs["pose_y"][ie], agent_obs["pose_theta"][ie]])
+                agent_pose = np.array(
+                    [
+                        agent_obs["pose_x"][ie],
+                        agent_obs["pose_y"][ie],
+                        agent_obs["pose_theta"][ie],
+                    ]
+                )
                 self.assertTrue(
                     np.allclose(agent_pose, rnd_poses[i]),
                     f"pose of agent {agent_id} in env {ie} should be {rnd_poses[i]}, got {agent_pose}",
