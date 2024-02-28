21a9e44646c202ccd67024f101a88ccb225e3360
Billy Zheng <billyzheng.bz@gmail.com>
2024-02-28 11:55:18 -0500

    update script for creating edt

--- examples/create_edt.py
+++ examples/create_edt.py
@@ -1,21 +1,43 @@
 from f110_gym.envs.track import Track
 from scipy.ndimage import distance_transform_edt as edt
-import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--track_name", type=str, required=True)
-args = parser.parse_args()
+DEFAULT_MAP_NAMES = [
+    "Austin",
+    "BrandsHatch",
+    "Budapest",
+    "Catalunya",
+    "Hockenheim",
+    "IMS",
+    "Melbourne",
+    "MexicoCity",
+    "Montreal",
+    "Monza",
+    "MoscowRaceway",
+    "Nuerburgring",
+    "Oschersleben",
+    "Sakhir",
+    "SaoPaulo",
+    "Sepang",
+    "Shanghai",
+    "Silverstone",
+    "Sochi",
+    "Spa",
+    "Spielberg",
+    "YasMarina",
+    "Zandvoort",
+]
 
-print("Loading a map without edt, a warning should appear")
-track = Track.from_track_name(args.track_name)
-occupancy_map = track.occupancy_map
-resolution = track.spec.resolution
+for track_name in DEFAULT_MAP_NAMES:
+    print("Loading a map without edt, a warning should appear")
+    track = Track.from_track_name(track_name)
+    occupancy_map = track.occupancy_map
+    resolution = track.spec.resolution
 
-dt = resolution * edt(occupancy_map)
+    dt = resolution * edt(occupancy_map)
 
-# saving
-np.save(track.filepath, dt)
+    # saving
+    np.save(track.filepath, dt)
 
-print("Loading a map with edt, warning should no longer appear")
-track_wedt = Track.from_track_name(args.track_name)
+    print("Loading a map with edt, warning should no longer appear")
+    track_wedt = Track.from_track_name(track_name)
