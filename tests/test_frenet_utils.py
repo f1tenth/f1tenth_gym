e6d95c95026ef24f66eb313dcecc7bbc44700e89
Ahmad Amine <AhmadAmine998@gmail.com>
2024-06-07 02:54:51 -0400

    Add test for calc arclength accurate and inaccurate

--- tests/test_frenet_utils.py
+++ tests/test_frenet_utils.py
@@ -42,7 +42,29 @@ class TestFrenet(unittest.TestCase):
         self.assertTrue(np.allclose(track.calc_position(np.pi), np.array([-1, 0]), atol=1e-3))
         self.assertTrue(np.allclose(track.calc_position(3 * np.pi / 2), np.array([0, -1]), atol=1e-3))
 
+    def test_calc_arclength(self):
+        circle_x = np.cos(np.linspace(0, 2 * np.pi, 100))[:-1]
+        circle_y = np.sin(np.linspace(0, 2 * np.pi, 100))[:-1]
+        track = cubic_spline.CubicSpline2D(circle_x, circle_y)
+        # Test the arclength at the four corners of the circle
+        self.assertAlmostEqual(track.calc_arclength(1, 0, 0)[0], 0, places=2)
+        self.assertAlmostEqual(track.calc_arclength(0, 1, 0)[0], np.pi/2, places=2)
+        self.assertAlmostEqual(track.calc_arclength(-1, 0, np.pi/2)[0], np.pi, places=2)
+        self.assertAlmostEqual(track.calc_arclength(0, -1, np.pi)[0], 3*np.pi/2, places=2)
+
+    def test_calc_arclength_inaccurate(self):
+        circle_x = np.cos(np.linspace(0, 2 * np.pi, 100))[:-1]
+        circle_y = np.sin(np.linspace(0, 2 * np.pi, 100))[:-1]
+        track = cubic_spline.CubicSpline2D(circle_x, circle_y)
+        # Test the arclength at the four corners of the circle
+        self.assertAlmostEqual(track.calc_arclength_inaccurate(1, 0)[0], 0, places=2)
+        self.assertAlmostEqual(track.calc_arclength_inaccurate(0, 1)[0], np.pi/2, places=2)
+        self.assertAlmostEqual(track.calc_arclength_inaccurate(-1, 0)[0], np.pi, places=2)
+        self.assertAlmostEqual(track.calc_arclength_inaccurate(0, -1)[0], 3*np.pi/2, places=2)
+
 if __name__ == "__main__":
     TestFrenet().test_calc_curvature()
     TestFrenet().test_calc_yaw()
     TestFrenet().test_calc_position()
+    TestFrenet().test_calc_arclength()
+    TestFrenet().test_calc_arclength_inaccurate()
