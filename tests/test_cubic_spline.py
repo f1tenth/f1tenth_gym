27914b8886528853b926b44dc5edcaabcbb2ea28
Ahmad Amine <AhmadAmine998@gmail.com>
2024-06-07 02:56:53 -0400

    REnamed frenet utils test to cubic spline test

--- tests/test_cubic_spline.py
+++ tests/test_cubic_spline.py
@@ -6,9 +6,9 @@ import numpy as np
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from f1tenth_gym.envs.track import Track, cubic_spline
+from f1tenth_gym.envs.track import cubic_spline
 
-class TestFrenet(unittest.TestCase):
+class TestCubicSpline(unittest.TestCase):
     def test_calc_curvature(self):
         circle_x = np.cos(np.linspace(0, 2 * np.pi, 100))[:-1]
         circle_y = np.sin(np.linspace(0, 2 * np.pi, 100))[:-1]
@@ -61,10 +61,3 @@ class TestFrenet(unittest.TestCase):
         self.assertAlmostEqual(track.calc_arclength_inaccurate(0, 1)[0], np.pi/2, places=2)
         self.assertAlmostEqual(track.calc_arclength_inaccurate(-1, 0)[0], np.pi, places=2)
         self.assertAlmostEqual(track.calc_arclength_inaccurate(0, -1)[0], 3*np.pi/2, places=2)
-
-if __name__ == "__main__":
-    TestFrenet().test_calc_curvature()
-    TestFrenet().test_calc_yaw()
-    TestFrenet().test_calc_position()
-    TestFrenet().test_calc_arclength()
-    TestFrenet().test_calc_arclength_inaccurate()
