67d72cf10c35842392d23161ee4b50131d595d28
Nandan Tumu <nandan.t@live.com>
2024-10-02 19:48:52 +0000

    Set environment variable

--- f1tenth_gym/envs/rendering/rendering_pyqt.py
+++ f1tenth_gym/envs/rendering/rendering_pyqt.py
@@ -3,6 +3,7 @@ import logging
 import math
 from typing import Any, Callable, Optional
 import signal
+import os
 
 import cv2
 import numpy as np
@@ -164,6 +165,7 @@ class PyQtEnvRenderer(EnvRenderer):
             signal.signal(signal.SIGINT, signal.SIG_DFL)
             self.window.show()
         elif self.render_mode == "rgb_array":
+            os.environ["QT_QPA_PLATFORM"] = "offscreen"
             self.exporter = ImageExporter(self.canvas)
 
     def update(self, state: dict) -> None:
