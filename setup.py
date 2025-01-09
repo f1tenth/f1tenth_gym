1a265256ad687ac31b60b032540ac98a2b040ec6
Aly Ashour <alyashour1@gmail.com>
2025-01-09 16:22:17 -0500

    update pyglet version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from distutils.core import setup
 from setuptools import setup
 
 setup(name='f110_gym',
@@ -12,6 +13,6 @@ setup(name='f110_gym',
                         'scipy>=1.7.3',
                         'numba>=0.55.2',
                         'pyyaml>=5.3.1',
-                        'pyglet<1.5',
+                        'pyglet==1.5.20',
                         'pyopengl']
       )
