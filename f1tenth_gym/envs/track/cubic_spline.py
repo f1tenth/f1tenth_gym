Fix calc yaw by using edge order 2 for np gradient

f1tenth_gym/envs/track/cubic_spline.py
@@ -144,8 +144,8 @@ class CubicSpline2D:
         return np.array(s)
     
     def _calc_yaw_from_xy(self, x, y):
-        dx_dt = np.gradient(x)
-        dy_dt = np.gradient(y)
+        dx_dt = np.gradient(x, edge_order=2)
+        dy_dt = np.gradient(y, edge_order=2)
         heading = np.arctan2(dy_dt, dx_dt)
         return heading
 
@@ -234,8 +234,7 @@ class CubicSpline2D:
         segment = self.find_segment_for_s(s)
         cos = self.predict_with_spline(s, segment, 2)[0]
         sin = self.predict_with_spline(s, segment, 3)[0]
-        # yaw = (math.atan2(sin, cos) + 2 * math.pi) % (2 * math.pi)
-        yaw = np.arctan2(sin, cos)
+        yaw = (math.atan2(sin, cos) + 2 * math.pi) % (2 * math.pi)
         return yaw
 
     def calc_arclength(